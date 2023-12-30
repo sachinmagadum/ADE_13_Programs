03c5193a36b2ac74c656043f08d5926d7e83e15a
sachinmagadum <sachinmagadum5009@gmail.com>
2023-12-30 18:14:31 +0530

    updated code for subtraction

--- addition.py
+++ addition.py
@@ -3,7 +3,14 @@ def addition(no1, no2):
     return ans
 
 
-x = 1000
+def subtraction(no1,no2):
+    ans = no1 - no2
+    return ans
+
+
+x = 5000
 y = 2000
 ans = addition(x,y)
 print(f"Addition of {x} and {y} is {ans}")
+ans = subtraction(x,y)
+print(f"subtraction of {x} and {y} is {ans}")
